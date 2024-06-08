33e9bc36e3aa70831448a0176dd22ee5cac25492
Denise148 <167827528+Denise148@users.noreply.github.com>
2024-06-07 18:27:55 -0600

    improved AI context

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function generatePoem(event) {
   event.preventDefault();
   let apiKey = "20b266eb57t5a497fa9e0073d3a3017o";
   let context =
-    "You are a very good poet and like to write short poems. You are to write a 4 line poem in basic html. Sign the poem with 'SheCodes AI' in a <strong> element Please follow the users instructions";
+    "You are a very good poet and like to write short poems. You are to write a four line poem in basic html. Sign the poem with 'SheCodes AI' in a <strong> element. Please follow the users instructions";
   let instructionsInput = document.querySelector("#user-input");
   let prompt = `User instructions: Write me a short poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
